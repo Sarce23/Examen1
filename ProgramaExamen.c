0cb442f4a686f7c6862f704c63990767193b151f
Sergio Arce <arces4246@gmail.com>
2018-10-11 14:43:17 -0700

    Programa Casi completo, falta la visualizacion del usuario

--- ProgramaExamen.c
+++ ProgramaExamen.c
@@ -2,9 +2,27 @@
 #include <stdlib.h>
 #include <math.h>
 
+int Suma(int a,int b);
+int Resta(int a,int b);
+int Multiplicacion(int a,int b);
+float Divicion(int a,int b);
+
 int main()
 {
+	int a;
+	int b;
+	int Resultado1,Resultado2,Resultado3;
+	float Resultado4;
+	pintf("incerte 2 numeros enteros:");
+	printf("inserte el primer numero:");
+	scanf("%d"&a);
+	printf("inserte el segundo numero:");
+	scanf("%d"&b);
 
+	Resultado1 = Suma(a,b);
+	Resultado2 = Resta(a,b);
+	Resultado3 = Multiplicacion(a,b);
+	Resultado4 = Divicion(a,b);
 	return 0;
 }
 
@@ -28,7 +46,6 @@ int Multiplicacion(int a,int b)
 	int resultado;
 	resultado= a * b;
 	return resultado;
-
 }
 
 float Divicion(int a,int b)
@@ -36,5 +53,4 @@ float Divicion(int a,int b)
 	int resultado;
 	resultado= a / b;
 	return resultado;
-	
 }
